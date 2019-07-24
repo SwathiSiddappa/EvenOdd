1aa2e388ac10a54514175a0c38d820e3b5a36aed
SwathiSiddappa <53049039+SwathiSiddappa@users.noreply.github.com>
2019-07-24 11:10:30 +0200

    Initial commit

---
+++ EvenOdd.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOdd
+{
+    class EvenOdd
+    {
+        public static void CheckEvenOdd()
+        {
+            try
+            {
+                Console.WriteLine("Enter a number : ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num % 2 == 0) Console.WriteLine("Number is Even");
+                else Console.WriteLine("Number is Odd");
+                Console.WriteLine("Do you want to Continue : Yes (y) / No (n) :");
+                string s = Console.ReadLine().ToLower();
+                switch (s)
+                {
+                    case "y":
+                        CheckEvenOdd();
+                        break;
+                    case "n":
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# EvenOdd
+ Program to check if number is even or odd
